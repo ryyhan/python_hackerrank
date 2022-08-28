9f58d323b612ff7e92f1c49208906d882c5d230a
Jbediah <prime1rehan@gmail.com>
2022-08-28 13:27:55 +0530

    Day 9

---
+++ Python Learning/backspaces.py
@@ -0,0 +1,10 @@
+
+
+""" This Program contains various escape sequences in python"""
+
+
+print("The Single quote \' . followed by the forward slash \\.")
+
+print("A new \n line and a carriage \r return")
+
+print("Not sure what a form feed \f does")
