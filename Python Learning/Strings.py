7765cc94da3272b9dea62e0474700af18c4f2992
Jbediah <prime1rehan@gmail.com>
2022-08-28 17:12:45 +0530

    Day 9

---
+++ Python Learning/Strings.py
@@ -0,0 +1,15 @@
+#String slicing
+
+sample="A quick brown fox jumps over a lazy dog"
+print(sample[:])
+
+#slicing with gaps
+
+print(sample[0:len(sample):2])
+
+#negative index
+
+print(sample[::-1])
+print(sample[-10:-1])
+
+#functions
---
+++ Python Learning/list_tuple.py
@@ -0,0 +1,17 @@
+#playing with lists
+
+fields=["Machine Learning","Data Science","Cyber Security","Data Analytics"]
+
+#print(fields.sort())#not printing anything
+#fields.push("w")
+#s=fields.sort()
+print(fields)
+
+
+#tuples
+"""just a single thing to make sure if the tuple is containing a single element
+then make sure a comma is added after it so that python knows its a tuple
+otherwise it will mistook it for a variable
+"""
+tup=(4,)
+print(type(tup))
