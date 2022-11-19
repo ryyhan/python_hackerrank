bd2df6c6d060140bc8e969718a5dd9580d0ecfc9
Jbediah <prime1rehan@gmail.com>
2022-11-19 18:11:28 +0530

    CS50

---
+++ Python Learning/CS50/ascii.py
@@ -0,0 +1,15 @@
+# python program to print ASCII
+# value of a given character
+
+# Assigning character to a variable
+char_var = 'A'
+# printing ASCII code
+print("ASCII value of " + char_var + " is = ", ord(char_var))
+
+char_var = 'x'
+# printing ASCII code
+print("ASCII value of " + char_var + " is = ", ord(char_var))
+
+char_var = '9'
+# printing ASCII code
+print("ASCII value of " + char_var + " is = ", ord(char_var))
