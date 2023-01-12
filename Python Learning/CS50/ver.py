ad7b12ebbd2530d6711af6a293d79588cca42f83
Jbediah <prime1rehan@gmail.com>
2023-01-12 22:20:37 +0530

    CS50

---
+++ Python Learning/CS50/ver.py
@@ -0,0 +1,14 @@
+# Python program to print the version information
+
+# importing the module
+import sys
+
+# printing version info
+print("Python version: ", sys.version)
+print("Python version info: ", sys.version_info)
+
+print("Python version info[0]: ", sys.version_info[0])
+print("Python version info[1]: ", sys.version_info[1])
+print("Python version info[2]: ", sys.version_info[2])
+print("Python version info[3]: ", sys.version_info[3])
+print("Python version info[4]: ", sys.version_info[4])
